1ebda277f82df721fa92db9dee6ba1afccd45550
Dillon <dillon@DillondeMacBook-Pro.local>
2018-10-07 23:50:07 +0800

    update to 1.6.0.

--- app/renderer/0.js
+++ app/renderer/0.js
@@ -1,49 +1,335 @@
 webpackJsonp([0],{
 
-/***/ "./node_modules/css-loader/index.js?{\"modules\":false,\"importLoaders\":2,\"minimize\":true,\"sourceMap\":true}!./node_modules/less-loader/dist/cjs.js!./src/renderer/assets/styles/search.less":
-/***/ (function(module, exports, __webpack_require__) {
+/***/ "./node_modules/monaco-editor/esm/vs/basic-languages/cpp/cpp.js":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-exports = module.exports = __webpack_require__("./node_modules/css-loader/lib/css-base.js")(true);
-// imports
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "conf", function() { return conf; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "language", function() { return language; });
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 
+var conf = {
+    comments: {
+        lineComment: '//',
+        blockComment: ['/*', '*/'],
+    },
+    brackets: [
+        ['{', '}'],
+        ['[', ']'],
+        ['(', ')']
+    ],
+    autoClosingPairs: [
+        { open: '[', close: ']' },
+        { open: '{', close: '}' },
+        { open: '(', close: ')' },
+        { open: '\'', close: '\'', notIn: ['string', 'comment'] },
+        { open: '"', close: '"', notIn: ['string'] },
+    ],
+    surroundingPairs: [
+        { open: '{', close: '}' },
+        { open: '[', close: ']' },
+        { open: '(', close: ')' },
+        { open: '"', close: '"' },
+        { open: '\'', close: '\'' },
+    ],
+    folding: {
+        markers: {
+            start: new RegExp("^\\s*#pragma\\s+region\\b"),
+            end: new RegExp("^\\s*#pragma\\s+endregion\\b")
+        }
+    }
+};
+var language = {
+    defaultToken: '',
+    tokenPostfix: '.cpp',
+    brackets: [
+        { token: 'delimiter.curly', open: '{', close: '}' },
+        { token: 'delimiter.parenthesis', open: '(', close: ')' },
+        { token: 'delimiter.square', open: '[', close: ']' },
+        { token: 'delimiter.angle', open: '<', close: '>' }
+    ],
+    keywords: [
+        'abstract',
+        'amp',
+        'array',
+        'auto',
+        'bool',
+        'break',
+        'case',
+        'catch',
+        'char',
+        'class',
+        'const',
+        'constexpr',
+        'const_cast',
+        'continue',
+        'cpu',
+        'decltype',
+        'default',
+        'delegate',
+        'delete',
+        'do',
+        'double',
+        'dynamic_cast',
+        'each',
+        'else',
+        'enum',
+        'event',
+        'explicit',
+        'export',
+        'extern',
+        'false',
+        'final',
+        'finally',
+        'float',
+        'for',
+        'friend',
+        'gcnew',
+        'generic',
+        'goto',
+        'if',
+        'in',
+        'initonly',
+        'inline',
+        'int',
+        'interface',
+        'interior_ptr',
+        'internal',
+        'literal',
+        'long',
+        'mutable',
+        'namespace',
+        'new',
+        'noexcept',
+        'nullptr',
+        '__nullptr',
+        'operator',
+        'override',
+        'partial',
+        'pascal',
+        'pin_ptr',
+        'private',
+        'property',
+        'protected',
+        'public',
+        'ref',
+        'register',
+        'reinterpret_cast',
+        'restrict',
+        'return',
+        'safe_cast',
+        'sealed',
+        'short',
+        'signed',
+        'sizeof',
+        'static',
+        'static_assert',
+        'static_cast',
+        'struct',
+        'switch',
+        'template',
+        'this',
+        'thread_local',
+        'throw',
+        'tile_static',
+        'true',
+        'try',
+        'typedef',
+        'typeid',
+        'typename',
+        'union',
+        'unsigned',
+        'using',
+        'virtual',
+        'void',
+        'volatile',
+        'wchar_t',
+        'where',
+        'while',
+        '_asm',
+        '_based',
+        '_cdecl',
+        '_declspec',
+        '_fastcall',
+        '_if_exists',
+        '_if_not_exists',
+        '_inline',
+        '_multiple_inheritance',
+        '_pascal',
+        '_single_inheritance',
+        '_stdcall',
+        '_virtual_inheritance',
+        '_w64',
+        '__abstract',
+        '__alignof',
+        '__asm',
+        '__assume',
+        '__based',
+        '__box',
+        '__builtin_alignof',
+        '__cdecl',
+        '__clrcall',
+        '__declspec',
+        '__delegate',
+        '__event',
+        '__except',
+        '__fastcall',
+        '__finally',
+        '__forceinline',
+        '__gc',
+        '__hook',
+        '__identifier',
+        '__if_exists',
+        '__if_not_exists',
+        '__inline',
+        '__int128',
+        '__int16',
+        '__int32',
+        '__int64',
+        '__int8',
+        '__interface',
+        '__leave',
+        '__m128',
+        '__m128d',
+        '__m128i',
+        '__m256',
+        '__m256d',
+        '__m256i',
+        '__m64',
+        '__multiple_inheritance',
+        '__newslot',
+        '__nogc',
+        '__noop',
+        '__nounwind',
+        '__novtordisp',
+        '__pascal',
+        '__pin',
+        '__pragma',
+        '__property',
+        '__ptr32',
+        '__ptr64',
+        '__raise',
+        '__restrict',
+        '__resume',
+        '__sealed',
+        '__single_inheritance',
+        '__stdcall',
+        '__super',
+        '__thiscall',
+        '__try',
+        '__try_cast',
+        '__typeof',
+        '__unaligned',
+        '__unhook',
+        '__uuidof',
+        '__value',
+        '__virtual_inheritance',
+        '__w64',
+        '__wchar_t'
+    ],
+    operators: [
+        '=', '>', '<', '!', '~', '?', ':',
+        '==', '<=', '>=', '!=', '&&', '||', '++', '--',
+        '+', '-', '*', '/', '&', '|', '^', '%', '<<',
+        '>>', '>>>', '+=', '-=', '*=', '/=', '&=', '|=',
+        '^=', '%=', '<<=', '>>=', '>>>='
+    ],
+    // we include these common regular expressions
+    symbols: /[=><!~?:&|+\-*\/\^%]+/,
+    escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+    integersuffix: /(ll|LL|u|U|l|L)?(ll|LL|u|U|l|L)?/,
+    floatsuffix: /[fFlL]?/,
+    encoding: /u|u8|U|L/,
+    // The main tokenizer for our languages
+    tokenizer: {
+        root: [
+            // C++ 11 Raw String
+            [/@encoding?R\"(?:([^ ()\\\t]*))\(/, { token: 'string.raw.begin', next: '@raw.$1' }],
+            // identifiers and keywords
+            [/[a-zA-Z_]\w*/, {
+                    cases: {
+                        '@keywords': { token: 'keyword.$0' },
+                        '@default': 'identifier'
+                    }
+                }],
+            // whitespace
+            { include: '@whitespace' },
+            // [[ attributes ]].
+            [/\[\[.*\]\]/, 'annotation'],
+            [/^\s*#include/, { token: 'keyword.directive.include', next: '@include' }],
+            // Preprocessor directive
+            [/^\s*#\s*\w+/, 'keyword'],
+            // delimiters and operators
+            [/[{}()\[\]]/, '@brackets'],
+            [/[<>](?!@symbols)/, '@brackets'],
+            [/@symbols/, {
+                    cases: {
+                        '@operators': 'delimiter',
+                        '@default': ''
+                    }
+                }],
+            // numbers
+            [/\d*\d+[eE]([\-+]?\d+)?(@floatsuffix)/, 'number.float'],
+            [/\d*\.\d+([eE][\-+]?\d+)?(@floatsuffix)/, 'number.float'],
+            [/0[xX][0-9a-fA-F']*[0-9a-fA-F](@integersuffix)/, 'number.hex'],
+            [/0[0-7']*[0-7](@integersuffix)/, 'number.octal'],
+            [/0[bB][0-1']*[0-1](@integersuffix)/, 'number.binary'],
+            [/\d[\d']*\d(@integersuffix)/, 'number'],
+            [/\d(@integersuffix)/, 'number'],
+            // delimiter: after number because of .\d floats
+            [/[;,.]/, 'delimiter'],
+            // strings
+            [/"([^"\\]|\\.)*$/, 'string.invalid'],
+            [/"/, 'string', '@string'],
+            // characters
+            [/'[^\\']'/, 'string'],
+            [/(')(@escapes)(')/, ['string', 'string.escape', 'string']],
+            [/'/, 'string.invalid']
+        ],
+        whitespace: [
+            [/[ \t\r\n]+/, ''],
+            [/\/\*\*(?!\/)/, 'comment.doc', '@doccomment'],
+            [/\/\*/, 'comment', '@comment'],
+            [/\/\/.*$/, 'comment'],
+        ],
+        comment: [
+            [/[^\/*]+/, 'comment'],
+            [/\*\//, 'comment', '@pop'],
+            [/[\/*]/, 'comment']
+        ],
+        //Identical copy of comment above, except for the addition of .doc
+        doccomment: [
+            [/[^\/*]+/, 'comment.doc'],
+            [/\*\//, 'comment.doc', '@pop'],
+            [/[\/*]/, 'comment.doc']
+        ],
+        string: [
+            [/[^\\"]+/, 'string'],
+            [/@escapes/, 'string.escape'],
+            [/\\./, 'string.escape.invalid'],
+            [/"/, 'string', '@pop']
+        ],
+        raw: [
+            [/(.*)(\))(?:([^ ()\\\t]*))(\")/, {
+                    cases: {
+                        '$3==$S2': ['string.raw', 'string.raw.end', 'string.raw.end', { token: 'string.raw.end', next: '@pop' }],
+                        '@default': ['string.raw', 'string.raw', 'string.raw', 'string.raw']
+                    }
+                }
+            ],
+            [/.*/, 'string.raw']
+        ],
+        include: [
+            [/(\s*)(<)([^<>]*)(>)/, ['', 'keyword.directive.include.begin', 'string.include.identifier', { token: 'keyword.directive.include.end', next: '@pop' }]],
+            [/(\s*)(")([^"]*)(")/, ['', 'keyword.directive.include.begin', 'string.include.identifier', { token: 'keyword.directive.include.end', next: '@pop' }]]
+        ]
+    },
+};
 
-// module
-exports.push([module.i, ".ant-card-body{padding:2px 10px!important}.ant-select-dropdown-menu{max-height:543px!important}.snippet-search{background:transparent!important}.snippet-search .ant-card-body{padding:5px 12px!important}.snippet-search .select-item{cursor:pointer}.snippet-search .select-item .select-item-desc{color:\"rgba(0,0,0,.65)\";width:130;font-weight:400;text-overflow:ellipsis;white-space:nowrap;outline:none;overflow:hidden}.snippet-search .select-item .select-item-tag{float:right;background:#8a2be2}", "", {"version":3,"sources":["/Users/dillon/once/once/src/renderer/assets/styles/search.less"],"names":[],"mappings":"AAAA,eACE,0BAA6B,CAC9B,AACD,0BACE,0BAA6B,CAC9B,AACD,gBACE,gCAAwC,CACzC,AACD,+BACE,0BAA6B,CAC9B,AACD,6BACE,cAAgB,CACjB,AACD,+CACE,wBAAyB,AACzB,UAAW,AACX,gBAAiB,AACjB,uBAAwB,AACxB,mBAAoB,AACpB,aAAc,AACd,eAAiB,CAClB,AACD,8CACE,YAAa,AACb,kBAAuB,CACxB","file":"search.less","sourcesContent":[".ant-card-body {\n  padding: 2px 10px !important;\n}\n.ant-select-dropdown-menu {\n  max-height: 543px !important;\n}\n.snippet-search {\n  background: rgba(0, 0, 0, 0) !important;\n}\n.snippet-search .ant-card-body {\n  padding: 5px 12px !important;\n}\n.snippet-search .select-item {\n  cursor: pointer;\n}\n.snippet-search .select-item .select-item-desc {\n  color: \"rgba(0,0,0,.65)\";\n  width: 130;\n  font-weight: 400;\n  text-overflow: ellipsis;\n  white-space: nowrap;\n  outline: none;\n  overflow: hidden;\n}\n.snippet-search .select-item .select-item-tag {\n  float: right;\n  background: blueviolet;\n}\n"],"sourceRoot":""}]);
-
-// exports
-
-
-/***/ }),
-
-/***/ "./src/renderer/assets/styles/search.less":
-/***/ (function(module, exports, __webpack_require__) {
-
-// style-loader: Adds some css to the DOM by adding a <style> tag
-
-// load the styles
-var content = __webpack_require__("./node_modules/css-loader/index.js?{\"modules\":false,\"importLoaders\":2,\"minimize\":true,\"sourceMap\":true}!./node_modules/less-loader/dist/cjs.js!./src/renderer/assets/styles/search.less");
-if(typeof content === 'string') content = [[module.i, content, '']];
-// Prepare cssTransformation
-var transform;
-
-var options = {}
-options.transform = transform
-// add the styles to the DOM
-var update = __webpack_require__("./node_modules/style-loader/addStyles.js")(content, options);
-if(content.locals) module.exports = content.locals;
-// Hot Module Replacement
-if(false) {
-	// When the styles change, update the <style> tags
-	if(!content.locals) {
-		module.hot.accept("!!../../../../node_modules/css-loader/index.js??ref--8-2!../../../../node_modules/less-loader/dist/cjs.js!./search.less", function() {
-			var newContent = require("!!../../../../node_modules/css-loader/index.js??ref--8-2!../../../../node_modules/less-loader/dist/cjs.js!./search.less");
-			if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
-			update(newContent);
-		});
-	}
-	// When the module is disposed, remove the <style> tags
-	module.hot.dispose(function() { update(); });
-}
 
 /***/ })
 
